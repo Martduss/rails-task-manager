e3eba223be03d51bdce612d19da338bd6b6cb05c
martin dussenneMartin Dussenne <martin.dussenne@gmail.com>
2025-11-04 11:35:23 +0100

    ajout de Read all and Read one

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  get "tasks", to: "tasks#index"
+
+  get "tasks/:id", to: "tasks#show", as: "task"
+
 end
